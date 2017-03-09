571a0311e9b59e952e076a7039dc05fe5bdddd78
NathalieMasudi\Nathalie Masudi <nathalie.masudi@student.unamur.be>
2017-03-09 18:31:42 +0100

    ...

---
+++ b314-compiler/src/main/java/be/unamur/info/b314/compiler/exception/ParsingException.java
@@ -0,0 +1,17 @@
+package be.unamur.info.b314.compiler.exception;
+
+/**
+ * Exception class representing parsing errors.
+ * @author Xavier Devroey - xavier.devroey@unamur.be
+ */
+public class ParsingException extends Exception {
+
+    public ParsingException(String message, Exception e) {
+        super(message, e);
+    }
+    
+    public ParsingException(String message){
+        super(message);
+    }
+
+}
---
+++ b314-compiler/src/main/java/be/unamur/info/b314/compiler/scope/SymTableFiller.java
@@ -0,0 +1,11 @@
+package be.unamur.info.b314.compiler.scope;
+
+import be.unamur.info.b314.compiler.B314BaseListener;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * Created by Nathalie Masudi on 08/03/2017.
+ */
+public class SymTableFiller extends B314BaseListener {
+
+}
